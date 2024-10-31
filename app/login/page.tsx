69db8f8a5c05ed143fa47ea44e03c6a6e772bf47
Denys Tryfonov <trifonowden123@gmail.com>
2024-10-31 17:59:27 +0200

    add skeleton

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,16 @@
 import { title } from "@/components/primitives";
+import { Input } from "@/components/ui/input";
 
 export default function SpeakersPage() {
 	return (
 		<div>
-			<h1 className={title()}>Спікери</h1>
+			<h1 className={title()}>Log</h1>
+			<h2>Create an account</h2>
+			<p></p>
+			<form action="">
+				<Input type="email" placeholder="Email" />
+				<Input type="email" placeholder="Email"/>
+		</form>
 		</div>
 	);
 }
