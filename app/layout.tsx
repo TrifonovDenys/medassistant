fb09ef49a24df2318152c687103f873b8f144be9
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2025-01-08 00:04:46 +0200

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,61 +1,48 @@
-import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "./providers";
-import { Link } from "@nextui-org/link";
+import '@/styles/globals.css';
+import { Metadata, Viewport } from 'next';
+import { siteConfig } from '@/config/site';
+import { fontSans } from '@/config/fonts';
+import { Providers } from './providers';
+import { Link } from '@nextui-org/link';
 
-
-
-import clsx from "clsx";
-import { Header } from "@/components/header/header";
-import Footer from "@/components/footer";
+import clsx from 'clsx';
+import { Header } from '@/components/header/header';
+import Footer from '@/components/footer';
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-icon-57x57.png",
-	},
-	manifest: "/manifest.json"
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-icon-57x57.png',
+  },
+  manifest: '/manifest.json',
 };
 
 export const viewport: Viewport = {
-themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-}
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+};
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
-			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
-						<Header />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-							{children}
-						</main>
-						<Footer/>
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <head />
+      <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+          <Header />
+          <div className='relative flex flex-col h-screen mx-auto max-w-7xl pt-16 px-6'>
+            <main className='container flex-grow'>{children}</main>
+            <Footer />
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
